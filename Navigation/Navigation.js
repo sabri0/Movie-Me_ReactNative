40ac8149f6e0461b493edbf6af571da0bca1d561
Sabri Barbaria <sabribarbar@gmail.com>
2021-08-31 18:56:51 +0100

    nav button

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,18 +1,62 @@
 
+import React from 'react' // N'oubliez pas l'import de React ici. On en a besoin pour rendre nos components React Native Image ! 
+import { StyleSheet, Image } from 'react-native';
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import Favorites from '../Components/Favorites'
 import Search from '../Components/Search'
 import FilmDetail from "../Components/FilmDetail";
 const SearchStackNavigator = createStackNavigator({
-  Search: { // Ici j'ai appelé la vue "Search" mais on peut mettre ce que l'on veut. C'est le nom qu'on utilisera pour appeler cette vue
+  Search: {
     screen: Search,
     navigationOptions: {
       title: 'Rechercher'
     }
   },
-  FilmDetail:{
+  FilmDetail: {
     screen: FilmDetail
   }
 })
 
-export default createAppContainer(SearchStackNavigator)
+const MoviesTabNavigator = createBottomTabNavigator(
+  {
+    Search: {
+      screen: SearchStackNavigator,
+      navigationOptions: {
+        tabBarIcon: () => { // On définit le rendu de nos icônes par les images récemment ajoutés au projet
+          return <Image
+            source={require('../images/ic_search.png')}
+            style={styles.icon}/> // On applique un style pour les redimensionner comme il faut
+        }
+      }
+    },
+    Favorites: {
+      screen: Favorites,
+      navigationOptions: {
+        tabBarIcon: () => {
+          return <Image
+            source={require('../images/ic_favorite.png')}
+            style={styles.icon}/>
+        }
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeBackgroundColor: '#DDDDDD', // Couleur d'arrière-plan de l'onglet sélectionné
+      inactiveBackgroundColor: '#FFFFFF', // Couleur d'arrière-plan des onglets non sélectionnés
+      showLabel: false, // On masque les titres
+      showIcon: true // On informe le TabNavigator qu'on souhaite afficher les icônes définis
+    }
+  }
+)
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30
+  }
+})
+
+export default createAppContainer(MoviesTabNavigator)
